b53817b2ff1e60c465491c4f9b32c37b94f3f5c4
zhouhaifang666 <zhouhaifang666@users.noreply.github.com>
2025-10-14 21:43:17 +0800

    添加时间显示功能和staic静态文件夹

--- hellowapp/views.py
+++ hellowapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def hello_world(request):
-    return HttpResponse("姓名：刘振兴  学号：20231201047  班级：23医信")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return HttpResponse(f"姓名：刘振兴  学号：20231201099  班级：23医信<br>当前时间：{current_time}")
