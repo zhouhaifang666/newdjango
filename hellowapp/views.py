0e8a11c689bfefbd679ed18a9233abe45a4a46fe
zhouhaifang666 <zhouhaifang666@users.noreply.github.com>
2025-10-19 16:50:37 +0800

    fix: 更正学生学号为20231201047

--- hellowapp/views.py
+++ hellowapp/views.py
@@ -4,4 +4,4 @@ from datetime import datetime
 
 def hello_world(request):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return HttpResponse(f"姓名：刘振兴  学号：20231201099  班级：23医信<br>当前时间：{current_time}")
+    return HttpResponse(f"姓名：周海方  学号：20231201066  班级：23医信<br>当前时间：{current_time}")
